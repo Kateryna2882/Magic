5c52afe6355a33ea47f55e140d004ac20cd7b252
Kateryna2882 <govoryhina82@gmail.com>
2024-11-22 18:03:52 +0200

    Add fields and methods

--- src/MagicJava.java
+++ src/MagicJava.java
@@ -1,2 +1,25 @@
 public class MagicJava {
-}
+//    public static void main(String[] args) {
+        int myNumber = 32;
+    /*
+    We will refer to myNumber as the original number
+     from now on - it might be helpful to document this.
+     */
+        public int Step() {
+            int stepOne = myNumber * myNumber;
+            int stepTwo = stepOne + myNumber;
+            int stepThree = stepTwo / myNumber;
+            int stepFour = stepThree + 17;
+            int stepFive = stepFour - myNumber;
+            return stepFive;
+//        System.out.println(stepFive);
+        }
+//        int stepOne = myNumber * myNumber;
+//        int stepTwo = stepOne + myNumber;
+//        int stepThree = stepTwo / myNumber;
+//        int stepFour = stepThree + 17;
+//        int stepFive = stepFour - myNumber;
+//        int StepFive = stepSix;
+//        System.out.println(stepFive);
+    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        MagicJava magic = new MagicJava();
+        int result = magic.Step();
+        System.out.println(result);
     }
+
 }
